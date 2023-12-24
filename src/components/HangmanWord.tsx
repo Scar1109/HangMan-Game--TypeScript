74ca66ffb44cd377cf91824f3dfd3fbbec8921ae
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2023-12-25 03:20:24 +0530

    Update HangmanWord.tsx

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,22 @@
 export function HangmanWord(){
-    
+    const word = "fuck"
+    const gussedLetters = ["u","c"]
+    return <div style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace"
+    }}>
+        {word.split("").map((letter, index) => (
+            <span style={{borderBottom : ".1em solid black" }} key = {index}>
+                <span style={{ visibility : gussedLetters.includes(letter) ? "visible" : "hidden"}}>
+                    {letter}
+                </span>
+            </span>
+            
+        ))}
+
+        </div>
 }
